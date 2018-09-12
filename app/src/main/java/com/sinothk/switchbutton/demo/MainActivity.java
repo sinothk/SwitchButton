29e95de45f603b7871bc281f10f7e61cd3e33267
LiangYutao <sinothk@126.com>
2018-09-12 14:40:26 +0800

    完成

--- app/src/main/java/com/sinothk/switchbutton/demo/MainActivity.java
+++ app/src/main/java/com/sinothk/switchbutton/demo/MainActivity.java
@@ -2,6 +2,9 @@ package com.sinothk.switchbutton.demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
+
+import com.sinothk.widget.switchButton.SwitchButton;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,20 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SwitchButton switchButton = (SwitchButton) findViewById(R.id.switch_button);
+//        switchButton.setChecked(true);
+//        switchButton.isChecked();
+//        switchButton.toggle();     //switch state
+//        switchButton.toggle(false);//switch without animation
+//        switchButton.setShadowEffect(true);//disable shadow effect
+//        switchButton.setEnabled(false);//disable button
+//        switchButton.setEnableEffect(false);//disable the switch animation
+        switchButton.setOnCheckedChangeListener(new SwitchButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(SwitchButton view, boolean isChecked) {
+                Toast.makeText(MainActivity.this, "" + isChecked, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
